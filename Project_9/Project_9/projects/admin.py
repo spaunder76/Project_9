afe9670c32e7f1f7b06179da6358a691355e3d45
spaunder76 <arthurbousquet36@gmail.com>
2024-07-03 19:22:36 +0200

    Delete Project_9 directory

--- Project_9/Project_9/projects/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Project, Contributor, Issue, Comment
-
-admin.site.register(Project)
-admin.site.register(Contributor)
-admin.site.register(Issue)
-admin.site.register(Comment)
--- Project_9/users/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import NewUser
-
-admin.site.register(NewUser)
